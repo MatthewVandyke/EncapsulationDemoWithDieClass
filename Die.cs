500034d9cfdaa2ea0e8a0a2dfedca864ec0a11c3
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-07-16 14:29:42 -0700

    finished die class

--- Die.cs
+++ Die.cs
@@ -7,21 +7,33 @@ using System.Threading.Tasks;
 namespace EncapsulationDemoWithDieClass
 {
 	/// <summary>
-	/// Represents an individual dice
+	/// Represents an individual standard dice
 	/// </summary>
 	class Die
 	{
 		// static fields are shared across all instances of a class
 		static Random rand;
+		byte minValue;
+		byte maxValue;
 
 		static Die() // Static constructors are called once for all instances
 		{
 			rand = new Random();
 		}
 
-		public Die()
+		public Die():this(1, 6){ }
+
+		/// <summary>
+		/// Creates a die with the numbers between the minimum
+		/// and maximum values
+		/// </summary>
+		/// <param name="minValue">The inclusive lower bound</param>
+		/// <param name="maxValue">The inclusive maximum bound</param>
+		public Die(byte minValue, byte maxValue)
 		{
-			Roll(); // Roll die on creation to generate first random number
+			this.minValue = minValue;
+			this.maxValue = maxValue;
+			Roll();
 		}
 
 		/// <summary>
@@ -33,11 +45,16 @@ namespace EncapsulationDemoWithDieClass
 
 		/// <summary>
 		/// Roll a new random value between 1 - 6
-		/// and return the newly rolled value
+		/// and return the newly rolled value.
+		/// If the die is held, the current will be returned
+		/// and no new value generated
 		/// </summary>
 		public byte Roll()
 		{
-			Value = (byte)rand.Next(1, 7);
+			if (!IsHeld) // If die is not held reroll die
+			{
+				Value = (byte)rand.Next(minValue, maxValue + 1); // +1 for exclusive random bound
+			}
 			return Value;
 		}
 	}
